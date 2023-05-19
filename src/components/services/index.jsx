330e6ba9ce32fe5a45b5edd9381cb3c12e2dd5eb
Vyacheslav <slava_badlaev1@mail.ru>
2023-05-19 10:14:51 +0300

    services++

--- src/components/services/index.jsx
+++ src/components/services/index.jsx
@@ -3,19 +3,44 @@ import ShowTextServices from '../ui/ShowTextServices'
 import ButtonServices from '../ui/ButtonServices'
 
 export default function Services() {
-  const items = [{path: 'multimodal', imgURL: 'multimodal.svg'}]
+  const items = [
+    {path: 'multimodal', imgURL: 'multimodal.svg', text: 'Мультимодальный сервис'},
+    {path: 'sea', imgURL: 'sea.svg', text: 'Морской сервис'},
+    {path: 'railway', imgURL: 'railway.svg', text: 'Железнодорожный сервис'},
+    {path: 'cargo', imgURL: 'cargo.svg', text: 'Сборные грузы'},
+    {path: 'cars', imgURL: 'cars.svg', text: 'Автомобильные перевозки'},
+    {path: 'air', imgURL: 'air.svg', text: 'Авиаперевозки'},
+    {path: 'rent', imgURL: 'rent.svg', text: 'Аренда контейнеров'},
+    {path: 'declaration', imgURL: 'declaration.svg', text: 'Таможенное оформление'},
+  ]
 
   return (
     <div >
-      <div style={{ backgroundImage: 'url(./icons/background/sevices.svg)', backgroundSize: 'cover', backgroundPosition: 'center', height:'525px'}}>
+      <div style={{ 
+        backgroundImage: 'url(./icons/background/sevices.svg)', 
+        backgroundSize: 'cover', 
+        backgroundPosition: 'center', height:'525px'
+        }}>
         <div className="container text-white" style={{ width: '1000px', marginBottom: '250px'}}>
-          <h1 className='bold-text' style={{ fontSize: '54px' , paddingTop: '15px'}}> AVALOG – это создание лучших логистических международных маршрутов под запрос клиента</h1>
+          <h1 className='bold-text' style={{ fontSize: '54px' , paddingTop: '15px'}}> 
+          AVALOG – это создание лучших логистических международных маршрутов под запрос клиента
+          </h1>
         </div>
       </div>
-      <div className='mb-5'>
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <ButtonServices items={items} />
-        <ShowTextServices title='Деятельность' text='Главной задачей компании является предоставление наиболее надежных и эффективных логистических решений в международных контейнерных перевозках' />
-        <ShowTextServices title='AVALOG' text='AVALOG - это опыт работы в Китае на протяжении многих лет, которые укрепили его позиции в основных направлениях международной логистики между Россией и Китаем, благодаря чему наши клиенты получают лучший сервис под любую конкретную задачу' />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '128px' }}>
+       <img src='./icons/services/serviseText.svg' alt='text' style={{ width: '1000px' }} />
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '128px' }}>
+       <img src='./icons/services/map.svg' alt='text' style={{ width: '1000px' }} />
+      </div>
+      {/* <div className='mb-5'> */}
+        {/* <ShowTextServices title='Преимущества личного кабинета' text='Главной задачей компании является предоставление наиболее надежных и эффективных логистических решений в международных контейнерных перевозках' /> */}
+      {/* </div> */}
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+       <img src='./icons/services/servicesText2.png' alt='text' style={{ width: '1000px' }} />
       </div>
     </div>
   )
--- src/components/ui/ButtonServices.jsx
+++ src/components/ui/ButtonServices.jsx
@@ -1,32 +1,33 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-
-// export default function ButtonServices( {items} ) {
-//   return (
-//     <div>
-      
-//       {items.map((item) => 
-//         <Link to={`/services/${item.path}`} 
-//         key={item.path}
-//         <img src=`{./icons/services/${item.imgURL}}` alt='arrow'/>'
-//         style={{backgroundImage: `url(./icons/services/${item.imgURL})`}} >О компании</Link>
-//       )}
-//     </div>
-//   )
-// }
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ButtonServices({ items }) {
   return (
-    <div>
-      {items.map((item) => (
-        <Link to={`/services/${item.path}`} key={item.path}>
-          <img src={`./icons/services/${item.imgURL}`} alt="arrow" />
-          О компании
-        </Link>
-      ))}
+    <div className="container">
+      <div className="row">
+        {items.map((item) => (
+          <div className="col-md-4" key={item.path}>
+            <Link
+              to={`/services/${item.path}`}
+              className="button-service"
+              style={{
+                textDecoration: 'none',
+                color: 'white',
+                fontSize: '24px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                marginTop: '30px',
+              }}
+            >
+              <div style={{ height: '100px', marginBottom: '10px' }}>
+                <img src={`./icons/services/${item.imgURL}`} alt="arrow" style={{ height: '100%' }} />
+              </div>
+              <div style={{ textAlign: 'center', maxWidth: '150px' }}>{item.text}</div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
